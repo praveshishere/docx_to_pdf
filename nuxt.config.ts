9e661648890bf667d399731635f2f3449d5c4b93
Prince Kunwar <prince.EY@tataaig.com>
2024-10-12 10:57:53 +0530

    Initial Setup

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    "nuxt-headlessui",
+  ],
+  devServer: {
+    port: 8080,
+  }
+})
